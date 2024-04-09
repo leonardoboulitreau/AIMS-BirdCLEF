9ecdb803085ef2fb3a5e6adeafbf0a058f1b715a
Leonardo Boulitreau <leonardoboulitreau@gmail.com>
2024-04-09 12:35:02 -0300

    add 2023 download script

--- input/birdclef-2023/download_birdclef2023.sh
+++ input/birdclef-2023/download_birdclef2023.sh
@@ -1,1 +1,6 @@
-#TO-DO
+#!/bin/bash
+pip install kaggle
+export KAGGLE_CONFIG_DIR=$1
+kaggle competitions download -c birdclef-2023 -p input/birdclef-2023
+unzip input/birdclef-2023/birdclef-2023.zip -d input/birdclef-2023
+rm input/birdclef-2023/birdclef-2023.zip
