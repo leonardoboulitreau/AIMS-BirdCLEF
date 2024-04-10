28c529c6006cb9fb864154951156841655f6ed38
Leonardo Boulitreau <leonardoboulitreau@gmail.com>
2024-04-10 16:30:55 -0300

    Create birdclef_container.sh

---
+++ birdclef_container.sh
@@ -0,0 +1,11 @@
+while getopts g:n:p: flag
+do
+        case "${flag}" in
+                g)  gpu=${OPTARG};;
+                n)  number=${OPTARG};;
+                p)  port=${OPTARG};;
+        esac
+done
+echo "Running container birdclef_container_$number on gpu $gpu and port $port";
+
+docker run --rm -it --gpus device=$gpu --userns=host --shm-size 64G -v /work/leonardo.boulitreau/AIMS-BirdCLEF:/workspace/aimsbirdclef/ -p $port --name birdclef_container_$number aism-birdclef:latest /bin/bash
