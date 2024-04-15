c9ae7271375f36fbc0721909848381accc3ec4e6
Leonardo Boulitreau <leonardoboulitreau@gmail.com>
2024-04-15 14:35:52 -0300

    Update birdclef_container.sh

--- birdclef_container.sh
+++ birdclef_container.sh
@@ -7,5 +7,4 @@ do
         esac
 done
 echo "Running container birdclef_container_$number on gpu $gpu and port $port";
-echo $PWD
 docker run --rm -it --gpus device=$gpu --userns=host --shm-size 64G -v /work/leonardo.boulitreau/kaggle-token-leo/:/workspace/kaggle-token-leo/ -v $PWD:/workspace/aimsbirdclef/ -p $port --name birdclef_container_$number aims-birdclef:latest /bin/bash
