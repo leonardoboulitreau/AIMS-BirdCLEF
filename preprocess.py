c9bb31fc9d2e6004852c623ceeb474a1516cfc4a
Leonardo Boulitreau <leonardo.boulitreau@dl-08.recod>
2024-05-09 13:57:42 -0300

    fix preprocessing

--- preprocess.py
+++ preprocess.py
@@ -17,31 +17,18 @@ def preprocess_train(trainval_df, preprocess_train_args, output_folder):
         #### Audio ####
         ###############
 
-        # Load
-        audio_data, sr = librosa.load(row_metadata['filepath'], sr=None)
-
         # Compute Windows
-        file_windows = []
-        if preprocess_train_args['audio_window'] == 'center':
-            n_copy = math.ceil(5 * sr / len(audio_data))
-            if n_copy > 1: audio_data = np.concatenate([audio_data]*n_copy)
-            start_idx = int(len(audio_data) / 2 - 2.5 * sr)
-            end_idx = int(start_idx + 5.0 * sr)
-            audio_data = audio_data[start_idx:end_idx]
-            file_windows.append(audio_data)
-        elif preprocess_train_args['audio_window'] == 'slicing':
-            raise NotImplementedError
-        else:
-            raise NotImplementedError
+        audio_windows = generate_audio_windows(row_metadata['filepath'], preprocess_train_args)
 
-        # Audio Processing (For each window: filtering, denoising, augmentation, etc)
+        # Audio Processing 
+        audio_windows = process_audio_windows(audio_windows, preprocess_train_args)
         
-        # Save Audios if wanted
+        # Save Audios
         if 'audio' in preprocess_train_args['output_feature_types']:
             audios_path = os.path.join(trainval_feat_path, 'audios')
             os.makedirs(audios_path, exist_ok=True)
-            for idx in range(len(file_windows)):
-                np.save(audios_path + '/' + row_metadata['filename'] + '_SEG' + str(idx) + '.npy', file_windows[idx])
+            for idx in range(len(audio_windows)):
+                np.save(audios_path + '/' + row_metadata['filename'] + '_SEG' + str(idx) + '.npy', audio_windows[idx])
 
 
         #####################
@@ -50,34 +37,9 @@ def preprocess_train(trainval_df, preprocess_train_args, output_folder):
 
         # Spectrogram Processing
         if 'spectrogram' in preprocess_train_args['output_feature_types']:
-            spectrograms = []
-            for window in file_windows:
-                mean_signal = np.nanmean(window)
-                window = np.nan_to_num(window, nan=mean_signal) if np.isnan(window).mean() < 1 else np.zeros_like(window)
-                
-                # Compute Spec
-                frequencies, times, spec_data = sci_signal.spectrogram(
-                    window, 
-                    fs=sr, 
-                    nfft= preprocess_train_args['spec_n_fft'], 
-                    nperseg= preprocess_train_args['spec_win_len'], 
-                    noverlap= preprocess_train_args['spec_hop_len'], 
-                    window= preprocess_train_args['spec_win_type']
-                )
-                
-                # Cut Frequencies
-                valid_freq = (frequencies >= preprocess_train_args['spec_cut_min_freq']) & (frequencies <= preprocess_train_args['spec_cut_max_freq'])
-                spec_data = spec_data[valid_freq, :]
-                
-                # Turn to Log
-                spec_data = np.log10(spec_data + 1e-20)
-                
-                # Normalize
-                spec_data = spec_data - spec_data.min()
-                spec_data = spec_data / spec_data.max()
-                spectrograms.append(spec_data)
+            spectrograms = generate_spectrograms(audio_windows, preprocess_train_args)
 
-            # Save Spectrograms if wanted
+            # Save Spectrograms
             specs_path = os.path.join(trainval_feat_path, 'spectrograms')
             os.makedirs(specs_path, exist_ok=True)
             for idx in range(len(spectrograms)):
@@ -87,15 +49,12 @@ def preprocess_train(trainval_df, preprocess_train_args, output_folder):
         #### Image ####
         ###############
 
-        # Image Processing
         if 'image' in preprocess_train_args['output_feature_types']:
-            images = []
-            for spec in spectrograms:
-                spec_data = cv2.resize(spec, (256, 256), interpolation=cv2.INTER_AREA)
-                images.append(spec_data)
 
-            # Save Images if wanted
-            
+            # Image Processing
+            images = generate_images(spectrograms, preprocess_train_args)            
+
+            # Save Images
             images_path = os.path.join(trainval_feat_path, 'images')
             os.makedirs(images_path, exist_ok=True)
             for idx in range(len(images)):
@@ -103,3 +62,60 @@ def preprocess_train(trainval_df, preprocess_train_args, output_folder):
 
     return trainval_feat_path
 
+
+def generate_audio_windows(path, preprocess_train_args):
+    audio_data, sr = librosa.load(path, sr=preprocess_train_args['sampling_rate'])
+    
+    file_windows = []
+    if preprocess_train_args['audio_window'] == 'center':
+        n_copy = math.ceil(5 * sr / len(audio_data))
+        if n_copy > 1: audio_data = np.concatenate([audio_data]*n_copy)
+        start_idx = int(len(audio_data) / 2 - 2.5 * sr)
+        end_idx = int(start_idx + 5.0 * sr)
+        audio_data = audio_data[start_idx:end_idx]
+        file_windows.append(audio_data)
+    elif preprocess_train_args['audio_window'] == 'slicing':
+        raise NotImplementedError
+    else:
+        raise NotImplementedError
+
+    return file_windows
+
+def process_audio_windows(audio_windows, preprocess_train_args):
+    return audio_windows
+
+def generate_spectrograms(audio_windows, preprocess_train_args):
+    spectrograms = []
+    for window in audio_windows:
+        mean_signal = np.nanmean(window)
+        window = np.nan_to_num(window, nan=mean_signal) if np.isnan(window).mean() < 1 else np.zeros_like(window)
+        
+        # Compute Spec
+        frequencies, times, spec_data = sci_signal.spectrogram(
+            window, 
+            fs=preprocess_train_args['sampling_rate'], 
+            nfft= preprocess_train_args['spec_n_fft'], 
+            nperseg= preprocess_train_args['spec_win_len'], 
+            noverlap= preprocess_train_args['spec_hop_len'], 
+            window= preprocess_train_args['spec_win_type']
+        )
+        
+        # Cut Frequencies
+        valid_freq = (frequencies >= preprocess_train_args['spec_cut_min_freq']) & (frequencies <= preprocess_train_args['spec_cut_max_freq'])
+        spec_data = spec_data[valid_freq, :]
+        
+        # Turn to Log
+        spec_data = np.log10(spec_data + 1e-20)
+        
+        # Normalize
+        spec_data = spec_data - spec_data.min()
+        spec_data = spec_data / spec_data.max()
+        spectrograms.append(spec_data)
+    return spectrograms
+
+def generate_images(spectrograms, preprocess_train_args):
+    images = []
+    for spec in spectrograms:
+        spec_data = cv2.resize(spec, (preprocess_train_args['img_size'], preprocess_train_args['img_size']), interpolation=cv2.INTER_AREA)
+        images.append(spec_data)
+    return images
--- run.py
+++ run.py
@@ -8,11 +8,11 @@ from dataframes import generate_dataframes, save_holdout, locate_training_files
 
 def main(args):
     
-    # LOAD CONFIG FROM .JSON
+    # LOAD CONFIG
     with open(args.config_path, 'r') as file:
         config_dict = json.load(file)
 
-    # PARSE SPECIFIC ARGS
+    # PARSE ARGS
     data_args = config_dict['data_args']
     preprocess_train_args = config_dict['preprocess_train_args']
     training_args = config_dict['training_args']
@@ -25,20 +25,24 @@ def main(args):
 
     # DATAFRAMES
     trainval_df, holdout_df = generate_dataframes(data_args)
+
+    # SAVE HOLDOUT
     if data_args['holdout_args']['use_holdout']:
         print('> Saving Holdout...')
         save_holdout(holdout_df, data_args, config_dict['output_folder'])
     
-    # LOCATE TRAIN FILES (Updates trainval dataframe, in case of multiple slices per audio)
+    # PREPROCESS OR LOAD
     if preprocess_train_args['run_preprocess']:
-        # Preprocess and Locate Train Files 
+        
         trainval_feat_path = preprocess_train(trainval_df, preprocess_train_args, config_dict['output_folder'])
-        trainval_df = locate_training_files(trainval_df, trainval_feat_path, training_args['features'])
     else:
-        # Locate Train Files    
-        trainval_df = locate_training_files(trainval_df, preprocess_train_args['loading_trainval_feats_path'], training_args['features']) 
+        trainval_feat_path = preprocess_train_args['loading_trainval_feats_path']
+    
+    # LOCATE    
+    trainval_df = locate_training_files(trainval_df, trainval_feat_path, training_args['features'])
 
     print(trainval_df)
+    
     # TRAINVAL SPLITS
     # if windowed assert grouped or grouped kfold to avoid vaazemtno
     # trainval_split_args = data_dict['trainval_split_args']
